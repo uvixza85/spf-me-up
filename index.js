dbf390e1aba10c4fdb01b9cf51f54cc764f49634
Yuvika Kapoor <yuvikakapoor08@gmail.com>
2025-04-16 22:08:56 +0530

    Add api route to fetch uv index from openuv api and display on frontend

--- index.js
+++ index.js
@@ -5,9 +5,19 @@ import bodyParser from "body-parser";
 const app = express()
 const port = 3000
 
+const config = {
+  headers: { "x-access-token":"openuv-3lxjgmrm9jzlt9i-io" },
+};
 
 app.get('/', async (req, res) => {
-    res.send("HEllo world");
+    try{
+    const result = await axios.get("https://api.openuv.io/api/v1/uv?lat=13.13&lng=77.59&alt=100", config);
+    res.render("index.ejs", { uvindex: result.data.result.uv });
+    console.log(result.data.result.uv);
+  } catch (error) {
+    console.log(error);
+    res.status(500)
+  }
 })
 
 app.listen(port, () => {
