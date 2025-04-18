6b526304930aff2fa66f1e9d222647aaf7e79b21
Yuvika Kapoor <yuvikakapoor08@gmail.com>
2025-04-19 01:02:24 +0530

    Add feature to fetch current location(latitude and longitude) using ipgeolocation.io API and use it to retrieve UV index

--- index.js
+++ index.js
@@ -14,7 +14,27 @@ const config = {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
-    res.render("index.ejs",{ uvindex: null });
+    res.render("home.ejs");
+})
+
+app.get('/cordi', async (req, res) => {
+    res.render("index.ejs");
+})
+app.get('/auto', async (req, res) => {
+    try {
+      const result1 = await axios.get("https://api.ip2location.io/");
+      const Lati=result1.data.latitude ;
+      const Longi  =result1.data.longitude ;
+      //console.log(Lati);
+      const result = await axios.get(`https://api.openuv.io/api/v1/uv?lat=${Lati}&lng=${Longi}&alt=100`, config);
+      const uvindex = result.data.result.uv ;
+      //console.log(uvindex);
+      res.render("index.ejs", { uvindex: uvindex });
+    } catch (error) {
+      console.log(error);
+      res.status(500)
+      
+    }
 })
 
 app.post("/submit", async (req, res) => {
