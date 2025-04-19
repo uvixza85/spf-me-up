a43fb44f75ebc7f9ce73adf1073bd40f08a443fb
Yuvika Kapoor <yuvikakapoor08@gmail.com>
2025-04-19 10:41:29 +0530

    change identifiers and semicolon

--- index.js
+++ index.js
@@ -1,61 +1,59 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-import 'dotenv/config'
+import 'dotenv/config';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.static('public'));
 
+// header configuration for openuv api
 const config = {
-  headers: { "x-access-token": process.env.OPEN_UV_ACCESS_TOKEN},
+  headers: { "x-access-token": process.env.OPEN_UV_ACCESS_TOKEN },
 };
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
-    res.render("home.ejs");
-})
+  res.render("home.ejs");
+});
 
 app.get('/cordi', async (req, res) => {
-    res.render("index.ejs");
-})
+  res.render("index.ejs");
+});
 
 app.get('/home', async (req, res) => {
-    res.render("home.ejs");
-})
+  res.render("home.ejs");
+});
 
 app.get('/auto', async (req, res) => {
-    try {
-      const result1 = await axios.get("https://api.ip2location.io/");
-      const Lati=result1.data.latitude ;
-      const Longi  =result1.data.longitude ;
-      //console.log(Lati);
-      const result = await axios.get(`https://api.openuv.io/api/v1/uv?lat=${Lati}&lng=${Longi}&alt=100`, config);
-      const uvindex = result.data.result.uv ;
-      //console.log(uvindex);
-      res.render("index.ejs", { uvindex: uvindex });
-    } catch (error) {
-      console.log(error);
-      res.status(500)
-    }
-})
-
-app.post("/submit", async (req, res) => {
-    const Lati = req.body.Lati;
-    const Longi = req.body.Longi;
-    try{
-    const result = await axios.get(`https://api.openuv.io/api/v1/uv?lat=${Lati}&lng=${Longi}&alt=100`, config);
-    const uvindex = result.data.result.uv ;
+  try {
+    const responseLocation = await axios.get("https://api.ip2location.io/");
+    const latitude = responseLocation.data.latitude;
+    const longitude = responseLocation.data.longitude;
+    const responseUvIndex = await axios.get(`https://api.openuv.io/api/v1/uv?lat=${latitude}&lng=${longitude}&alt=100`, config);
+    const uvindex = responseUvIndex.data.result.uv;
     res.render("index.ejs", { uvindex: uvindex });
   } catch (error) {
     console.log(error);
-    res.status(500)
+    res.status(500);
   }
 });
 
+app.post("/submit", async (req, res) => {
+  const latitude = req.body.latitude;
+  const longitude = req.body.longitude;
+  try {
+    const responseUvIndex = await axios.get(`https://api.openuv.io/api/v1/uv?lat=${latitude}&lng=${longitude}&alt=100`, config);
+    const uvindex = responseUvIndex.data.result.uv;
+    res.render("index.ejs", { uvindex: uvindex });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-})
+});
